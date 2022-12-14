c71e80f0bda5be496bba7e916de91a09e6f5f1e1
SandraLjubic <51881494+SandraLjubic@users.noreply.github.com>
2022-12-14 19:49:08 +0100

    Update wavToOnOff.py
    added error handling
    added that script can be called with parameter for path to wav

--- wavToOnOff.py
+++ wavToOnOff.py
@@ -4,10 +4,12 @@ import os
 import os.path 
 from os import path 
 from time import sleep 
+import sys 
 
+filePath=sys.argv[1]
 # POST wav file
 url = "http://192.168.8.106:12101/api/speech-to-intent"
-filePath = '/home/pi/Desktop/rec.wav'
+# filePath = '/home/pi/Desktop/rec.wav'
 
 headers = {
    # "acl": "public-read",
@@ -25,23 +27,32 @@ params = {"uploadType": "media", "name": "rec.wav"}
 while (True):
 	sleep (0.5)
 	if not path.isfile(filePath):
-		print ("kein File")
+		print ("waiting for file")
 		continue
-	with open(filePath, 'rb') as file:
-		r = requests.post(url, params=params, headers=headers, data=file)
-	# print(r.text)
+	try:
+		with open(filePath, 'rb') as file:
+			r = requests.post(url, params=params, headers=headers, data=file)
+		os.remove(filePath)
+	#	print("r.text")
+	#	print(r.text)
+		
 
-	#getting response
-	response=json.loads(r.text)
-	# print(response)
+		#getting response
+		response=json.loads(r.text)
+	#	print("response")
+	#	print(response)
 
-	on_off=response["entities"][0]["raw_value"]
-	print(on_off)
-	
-	os.remove(filePath)
-	
-	with open('/home/pi/Desktop/finalCommand.txt', 'w') as file:
-  		file.write(on_off)
+		on_off=response["entities"][0]["raw_value"]
+		print("got file with: " + on_off)
+		
+		
+		with open('/home/pi/Desktop/finalCommand.txt', 'w') as file:
+	  		file.write(on_off)
+	except:
+		with open('/home/pi/Desktop/finalCommand.txt', 'w') as file:
+	  		file.write("unknown error")
+	  		print("unknown error")
+	  		
+  
 
-	
-	
+
